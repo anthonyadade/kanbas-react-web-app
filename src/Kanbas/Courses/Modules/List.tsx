935c7b7f8a941cce0427ff9664b0c5f155ab2a01
anthonyadade <123226585+anthonyadade@users.noreply.github.com>
2024-03-21 11:12:37 -0400

    a4 State and Redux fa23

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,20 +1,86 @@
 import React, { useState } from "react";
 import "./index.css";
-import { modules } from "../../Database";
+import db from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    addModule,
+    deleteModule,
+    updateModule,
+    setModule,
+} from "./reducer";
+import { KanbasState } from "../../store";
 function ModuleList() {
     const { courseId } = useParams();
-    const modulesList = modules.filter((module) => module.course === courseId);
-    const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+    const moduleList = useSelector((state: KanbasState) =>
+        state.modulesReducer.modules);
+    const module = useSelector((state: KanbasState) =>
+        state.modulesReducer.module);
+    const dispatch = useDispatch();
+    /*const [moduleList, setModuleList] = useState<any[]>(db.modules);
+    //const modulesList = db.modules.filter((module) => module.course === courseId);
+    const [module, setModule] = useState({
+        name: "New Module",
+        description: "New Description",
+        course: courseId,
+        _id: "0",
+    });*/
+    /*const deleteModule = (moduleId: string) => {
+        const newModuleList = moduleList.filter(
+            (module) => module._id !== moduleId );
+        setModuleList(newModuleList);
+    };
+
+    const addModule = (module: any) => {
+        const newModule = { ...module,
+            _id: new Date().getTime().toString() };
+        const newModuleList = [newModule, ...moduleList];
+        setModuleList(newModuleList);
+    };
+    const updateModule = () => {
+        const newModuleList = moduleList.map((m) => {
+            if (m._id === module._id) {
+                return module;
+            } else {
+                return m;
+            }
+        });
+        setModuleList(newModuleList);
+    };*/
+
+
     return (
         <>
-            {/* <!-- Add buttons here --> */}
+            <li className="list-group-item d-flex">
+                <button className={"wd-modules-button"} onClick={() =>
+                    dispatch(addModule({ ...module, course: courseId }))}>Add</button>
+                <button className={"wd-modules-button"} onClick={() => dispatch(updateModule(module))}>
+                    Update
+                </button>
+
+                <input className={"wd-modules-inputs"} value={module.name}
+                       onChange={(e) =>
+                           dispatch(setModule({ ...module, name: e.target.value }))}
+                />
+                <textarea className={"wd-modules-inputs"} value={module.description}
+                          onChange={(e) => setModule({
+                              ...module, description: e.target.value })}
+                />
+            </li>
             <ul className="list-group wd-modules">
-                {modulesList.map((module, index) => (
+                {moduleList.filter((module) => module.course === courseId).map((module, index) => (
                     <li key={index}
                         className="list-group-item"
-                        onClick={() => setSelectedModule(module)}>
+                        onClick={() => setModule(module)}>
+                        <button className = "wd-modules-button"
+                                onClick={() => dispatch(setModule(module))}>
+                            Edit
+                        </button>
+                        <button className = "wd-modules-button"
+                                onClick={() => dispatch(deleteModule(module._id))}>
+                            Delete
+                        </button>
                         <div>
                             <FaEllipsisV className="me-2" />
                             {module.name}
@@ -24,9 +90,9 @@ function ModuleList() {
                 <FaEllipsisV className="ms-2" />
               </span>
                         </div>
-                        {selectedModule._id === module._id && (
+                        {module._id === module._id && (
                             <ul className="list-group">
-                                {module.lessons?.map((lesson, index) => (
+                                {module.lessons?.map((lesson: any, index: number) => (
                                     <li className="list-group-item" key={index}>
                                         <FaEllipsisV className="me-2" />
                                         {lesson.name}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,16 +1,26 @@
 import Assignment3 from "./a3";
 import Nav from "../Nav";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate} from "react-router-dom";
+import Assignment4 from "./a4";
+import store from "./store";
+import { Provider } from "react-redux";
 function Labs() {
     return (
+        <Provider store={store}>
         <div className="container-fluid">
             <h1>Labs</h1>
-            <Nav />
+            <Nav/>
             <Link to="/Labs/a3">Assignment 3</Link>
             <Routes>
-                <Route path="/a3/*" element={<Assignment3 />} />
+                <Route path="/"
+                       element={<Navigate
+                           to="a3"/>}/>
+                <Route path="/a3/*" element={<Assignment3/>}/>
+                <Route path="a4"
+                       element={<Assignment4/>}/>
             </Routes>
         </div>
+        </Provider>
     );
 }
 export default Labs;
